721f92ecb7b88c50526e67163b0ac4d254bc4b4b
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2025-03-05 04:44:12 +0300

    1

--- productids.js
+++ productids.js
@@ -1,102 +1,102 @@
 module.exports={
     
     
-const KADINid : "9c8c7c2a75b3c676";
-const AYAKKABIid : "bdce94fb95dd9db1";
+ KADINid : "9c8c7c2a75b3c676";
+ AYAKKABIid : "bdce94fb95dd9db1";
 
-const ÇANTAid : "6f6880c7106a0b37";
+ ÇANTAid : "6f6880c7106a0b37";
 
-const AKSESUARid : "51495dc1b7425b77";
+ AKSESUARid : "51495dc1b7425b77";
 
-const BOTid : "a028bb132a02daaa";
+ BOTid : "a028bb132a02daaa";
 
-const ÇİZMEid : "daf6c42241ab73ae";
+ ÇİZMEid : "daf6c42241ab73ae";
 
-const TOPUKLUid : "d4e8e12b8d4acf0a";
+ TOPUKLUid : "d4e8e12b8d4acf0a";
 
-const SNEAKERid : "fcb536309c0fc0ad";
+ SNEAKERid : "fcb536309c0fc0ad";
 
-const LOAFERid : "0c9726f6c32dafa8";
+ LOAFERid : "0c9726f6c32dafa8";
 
-const SPORAYAKKABIid : "747fc695c3418ef8";
+ SPORAYAKKABIid : "747fc695c3418ef8";
 
-const YÜRÜYÜŞid : "b891eec49ea4ace2";
+ YÜRÜYÜŞid : "b891eec49ea4ace2";
 
-const KOŞUid : "1952499236d237ea";
+ KOŞUid : "1952499236d237ea";
 
-const BABETid : "bb6f615d3356080b";
+ BABETid : "bb6f615d3356080b";
 
-const SANDALETid : "90d109df9461a6cf";
+ SANDALETid : "90d109df9461a6cf";
 
-const ÇAPRAZid : "785458d81fb0b298";
+ ÇAPRAZid : "785458d81fb0b298";
 
-const ELÇANTASIid : "0b8db0567a8a82df";
+ ELÇANTASIid : "0b8db0567a8a82df";
 
-const HASIRid : "efda71a715860529";
+ HASIRid : "efda71a715860529";
 
-const OMUZid : "4296fcc9d6cc13ef";
+ OMUZid : "4296fcc9d6cc13ef";
 
-const KOLÇANTASIid : "0111f4af1a801b83";
+ KOLÇANTASIid : "0111f4af1a801b83";
 
-const ATKIid : "134fdc424de0b935";
+ ATKIid : "134fdc424de0b935";
 
-const KÜPEid : "b2deafa09752b401";
+ KÜPEid : "b2deafa09752b401";
 
-const BEREid : "6bbfd1725f2ca491";
+ BEREid : "6bbfd1725f2ca491";
 
-const SAATid : "5bf616c78306fb15";
+ SAATid : "5bf616c78306fb15";
 
-const BİLEKLİKid : "ada093572986ec58";
+ BİLEKLİKid : "ada093572986ec58";
 
-const YÜZÜKid : "5681ea3be7719654";
+ YÜZÜKid : "5681ea3be7719654";
 
-const CÜZDANid : "03d01082d7419554";
+ CÜZDANid : "03d01082d7419554";
 
-const GÖZLÜid : "615bb27af6325400";
+ GÖZLÜid : "615bb27af6325400";
 
-const KOLYEid : "4202e7acda95f301";
+ KOLYEid : "4202e7acda95f301";
 
-const ADIDASid : "c7826925739164b2";
+ ADIDASid : "c7826925739164b2";
 
-const CONVERSEid : "7cfe8749ab9ae6e5";
+ CONVERSEid : "7cfe8749ab9ae6e5";
 
-const NIKEid : "9158b9d1390e7297";
+ NIKEid : "9158b9d1390e7297";
 
-const JORDANid : "2e8e1b3ea68d7032";
+ JORDANid : "2e8e1b3ea68d7032";
 
-const MCQUEENid : "27158d122dcad430";
+ MCQUEENid : "27158d122dcad430";
 
-const PARISid : "d7ee3aef1457381e";
+ PARISid : "d7ee3aef1457381e";
 
-const BALENCIAGid : "6a9b8ae494b7f685";
+ BALENCIAGid : "6a9b8ae494b7f685";
 
-const BOTTEGAid : "0fcd7e8e14821dc9";
+ BOTTEGAid : "0fcd7e8e14821dc9";
 
-const BVLGARIid : "a53c580b3b8abe76";
+ BVLGARIid : "a53c580b3b8abe76";
 
-const CHANELid : "180b726c3cc37a09";
+ CHANELid : "180b726c3cc37a09";
 
-const CELINEid : "30b112d9fbb6604d";
+ CELINEid : "30b112d9fbb6604d";
 
-const GOOSEid : "54746a624a15bc23";
+ GOOSEid : "54746a624a15bc23";
 
-const GOYARDid : "85deaa6306167356";
+ GOYARDid : "85deaa6306167356";
 
-const GUCCIid : "fa81888aad8d0f9a";
+ GUCCIid : "fa81888aad8d0f9a";
 
-const HERMESid : "90867aa79b51202c";
+ HERMESid : "90867aa79b51202c";
 
-const JACQUEMUSid : "3b67ce2f30adcd90";
+ JACQUEMUSid : "3b67ce2f30adcd90";
 
-const PIANAid : "a85d41f154450da8";
+ PIANAid : "a85d41f154450da8";
 
-const MIUid : "7fd953b21d329ce2";
+ MIUid : "7fd953b21d329ce2";
 
-const PRADAid : "cae93ef53349ecf3";
+ PRADAid : "cae93ef53349ecf3";
 
-const POLENEid : "ed7a46cc75c49904";
+ POLENEid : "ed7a46cc75c49904";
 
-const SAINTid : "76cf79a1f712f9cf";
+ SAINTid : "76cf79a1f712f9cf";
 
-const TOMFORDid : "befff5a702a09d16";
+ TOMFORDid : "befff5a702a09d16";
 }
