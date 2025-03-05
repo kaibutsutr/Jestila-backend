b9058d40165d27e58fec00898e62014ee419826e
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2025-03-05 04:35:49 +0300

    1

---
+++ controllers/womanshop.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+
+const asyncWrapper = require("../middleware/async");
+const { createCustomError } = require("../errors/custom-error");
+
+const getAllproducts = (req, res) => {
+  const options = {
+    method: "GET",
+    url: "https://api.shopier.com/v1/products?limit=50&page=1&sort=dateDesc",
+    headers: {
+      accept: "application/json",
+      authorization: Bearer,
+    },
+  };
+
+  axios
+    .request(options)
+    .then((response) => {
+      const contentType = response.headers["content-type"];
+      const data = response.data;
+      res.status(200).json({ data }); //send json object with success true and  array
+    })
+    .catch((err) => console.error(err));
+
+  //GET
+};
+
+module.exports = {
+  getAllproducts,
+}; // export and object with functions
