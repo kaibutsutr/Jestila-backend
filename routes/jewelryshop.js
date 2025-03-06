07cb36a97f36a28bdca765f0a9db9e887a846987
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2025-03-06 03:30:23 +0300

    12

--- routes/jewelryshop.js
+++ routes/jewelryshop.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllproducts } = require("../controllers/accessoriesshop");
+const { getAllproducts } = require("../controllers/jewelryshop");
 
 router.route("/").get(getAllproducts);
 
