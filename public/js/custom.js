a9ad657fe01415bce2f33cd254cc9061a2e3cf83
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2025-03-07 04:30:51 +0300

    12

--- public/js/custom.js
+++ public/js/custom.js
@@ -11,6 +11,112 @@ var discount = false;
 var list = false;
 url = "womanshop";
 
+const constants = {
+  Bearer:
+    "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5NDA1ODlkYmQyNTA5NTgzMDVkNjc5MTFiMjBiMDY5ZSIsImp0aSI6IjE2YjhiZjc2YmYxMDgyNDA4YjBjNDRjMTI3ZGM1NTRhYmY0Y2E3ZGE2M2NjMDM1MDBlYTllY2E1NDgzZTI4NTk3MWRhYjhhYTJlMWQ5MjA4NWM4N2U4NzJlYjY0OWY4ZjBkNDMxZTNkNWVmNzNlN2EwOTkzZTQ1NGM0OWJkMGM2YThkMWJiYWE5YmZkNTIxYTYwYTRmZTZiMzM0NDQ0NWYiLCJpYXQiOjE3NDAzOTQ2NTYsIm5iZiI6MTc0MDM5NDY1NiwiZXhwIjoxODk4MTc5NDE2LCJzdWIiOiIxODc5MTAyIiwic2NvcGVzIjpbIm9yZGVyczpyZWFkIiwib3JkZXJzOndyaXRlIiwicHJvZHVjdHM6cmVhZCIsInByb2R1Y3RzOndyaXRlIiwic2hpcHBpbmdzOnJlYWQiLCJzaGlwcGluZ3M6d3JpdGUiLCJkaXNjb3VudHM6cmVhZCIsImRpc2NvdW50czp3cml0ZSIsInBheW91dHM6cmVhZCIsInJlZnVuZHM6cmVhZCIsInJlZnVuZHM6d3JpdGUiLCJzaG9wOnJlYWQiLCJzaG9wOndyaXRlIl19.Bij3RbmTc_2xcYEdsVEOCw3lcQHzSmsD4x1lBrBgM01jjvg6g2IwyvWuLaAtgQYOZW-9kd37Nwc_hXWwZBnVEwICLaiwsPq6mUwUBIfhNCApUXJRxpLgnNMI5miIZp0m324Cev_qLVhLZcdup65nwiNKp6q3F_ypcNxnxykzPw1riSgkBUl9DEVmhviWt7eG_jopmIskG3zt6fou1aIFtNgm6q0rGpO6mUsOSK8AwC73UM9upDWtBlO3xB_grH40ZQ9fLhy5EZpM1QwEuEMBiJtp2acuEXGl-Z3NjS3Q6xivuzLsMfShMz6ov1xaf0FODOx3LpuoBBvNwYsCvTsjqg",
+
+  KADINAYAKKABIid: "97fa66025e772afe",
+  AYAKKABIid: "4d1670e409f3277d",
+
+  ÇANTAid: "6f6880c7106a0b37",
+
+  AKSESUARid: "51495dc1b7425b77",
+
+  TAKIid: "8a4bd2da24265fd0",
+
+  BOTid: "a028bb132a02daaa",
+
+  ÇİZMEid: "daf6c42241ab73ae",
+
+  TOPUKLUid: "d4e8e12b8d4acf0a",
+
+  SNEAKERid: "fcb536309c0fc0ad",
+
+  LOAFERid: "0c9726f6c32dafa8",
+
+  SPORAYAKKABIid: "747fc695c3418ef8",
+
+  YÜRÜYÜŞid: "b891eec49ea4ace2",
+
+  KOŞUid: "1952499236d237ea",
+
+  BABETid: "bb6f615d3356080b",
+
+  SANDALETid: "90d109df9461a6cf",
+
+  ÇAPRAZid: "785458d81fb0b298",
+
+  ELÇANTASIid: "0b8db0567a8a82df",
+
+  HASIRid: "efda71a715860529",
+
+  OMUZid: "4296fcc9d6cc13ef",
+
+  KOLÇANTASIid: "0111f4af1a801b83",
+
+  ATKIid: "134fdc424de0b935",
+
+  KÜPEid: "b2deafa09752b401",
+
+  BEREid: "6bbfd1725f2ca491",
+
+  SAATid: "5bf616c78306fb15",
+
+  BİLEKLİKid: "ada093572986ec58",
+
+  YÜZÜKid: "5681ea3be7719654",
+
+  CÜZDANid: "03d01082d7419554",
+
+  GÖZLÜid: "615bb27af6325400",
+
+  KOLYEid: "4202e7acda95f301",
+
+  ADIDASid: "c7826925739164b2",
+
+  CONVERSEid: "7cfe8749ab9ae6e5",
+
+  NIKEid: "9158b9d1390e7297",
+
+  JORDANid: "2e8e1b3ea68d7032",
+
+  MCQUEENid: "27158d122dcad430",
+
+  PARISid: "d7ee3aef1457381e",
+
+  BALENCIAGid: "6a9b8ae494b7f685",
+
+  BOTTEGAid: "0fcd7e8e14821dc9",
+
+  BVLGARIid: "a53c580b3b8abe76",
+
+  CHANELid: "180b726c3cc37a09",
+
+  CELINEid: "30b112d9fbb6604d",
+
+  GOOSEid: "54746a624a15bc23",
+
+  GOYARDid: "85deaa6306167356",
+
+  GUCCIid: "fa81888aad8d0f9a",
+
+  HERMESid: "90867aa79b51202c",
+
+  JACQUEMUSid: "3b67ce2f30adcd90",
+
+  PIANAid: "a85d41f154450da8",
+
+  MIUid: "7fd953b21d329ce2",
+
+  PRADAid: "cae93ef53349ecf3",
+
+  POLENEid: "ed7a46cc75c49904",
+
+  SAINTid: "76cf79a1f712f9cf",
+
+  TOMFORDid: "befff5a702a09d16",
+};
+
 /*------------ glasscase ---------*/
 
 $(function () {
@@ -27,10 +133,11 @@ $(function () {
   if (document.location.pathname == "/brand-shop.html") {
     var brandName = localStorage.getItem("brandName");
     console.log(brandName);
+    var brandID = constants[brandName];
+    console.log(brandID);
     url = "brandshop";
-    brandId = "c7826925739164b2";
 
-    getProductsByBrand(brandId);
+    getProductsByBrand(brandID);
     // hardcode constants here!
   }
   if (document.location.pathname == "/woman-shop.html") {
