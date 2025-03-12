c6edc74d208bb9cf66bf18f8ba7f86df41af83cb
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2025-03-12 08:55:38 +0300

    12

--- public/js/custom.js
+++ public/js/custom.js
@@ -2301,6 +2301,7 @@ const getProductOfTheWeek = async (productId) => {
 };
 
 // price sorting
+/* Grid Var*/
 const getPriceGrid = async (url) => {
   axios
     .get(`/api/v1/database/${url}`)
@@ -2383,4 +2384,128 @@ const getPriceGrid = async (url) => {
       console.error("Error fetching data:", error);
     });
 };
-/* Grid Var*/
+
+/* get as a List view*/
+
+const getProductsList = async () => {
+  axios
+    .get(`/api/v1/database/${url}`)
+    .then((response) => {
+      products = response.data;
+      console.log(products);
+      for (let object in products) {
+        for (let key in products[object]) {
+          $("#product").append(
+            $("<div>")
+              .addClass(
+                "featured-product featured-product-list align-flax mb-25"
+              )
+              .append(
+                $("<div>")
+                  .addClass("product-img transition")
+                  .attr({ id: products[object][key].id })
+                  .append(
+                    $("<a>")
+                      .attr({ href: "product-detail.html", id: "product-link" })
+                      .append(
+                        $("<img>").addClass("transition").attr({
+                          src: products[object][key].media[0].url,
+                          alt: "product",
+                          id: "product-link",
+                        })
+                      )
+                  )
+                  .append(
+                    $("<div>")
+                      .addClass("product-details-btn text-center transition")
+                      .attr({ id: products[object][key].id })
+                      .append(
+                        $("<a>")
+                          .addClass("quick-popup")
+
+                          .attr({
+                            href: "product-quick-view.html",
+                            id: "product-link",
+                          })
+                          .text("Ön İzleme")
+                      )
+                  )
+              )
+              .append(
+                $("<div>")
+                  .addClass("product-desc")
+                  .attr({ id: products[object][key].id })
+                  .append(
+                    $("<a>")
+                      .addClass("product-name")
+                      .attr({ href: "product-detail.html", id: "product-link" })
+                      .text(products[object][key].title)
+                  )
+                  .append(
+                    $("<span>")
+                      .addClass("product-price")
+                      .text(
+                        ~~products[object][key].priceData.discountedPrice + " ₺"
+                      )
+                  )
+                  .append(
+                    $("<div>")
+                      .addClass("product-info")
+                      .append(products[object][key].description)
+                  )
+                  .append(
+                    $("<div>")
+                      .addClass("product-action")
+                      .append(
+                        $("<ul>")
+                          .append(
+                            $("<li>").append(
+                              $("<a>")
+                                .addClass("quick-popup btn btn-color")
+                                .attr({
+                                  href: products[object][key].url,
+                                  id: "product-link",
+                                })
+
+                                .append(
+                                  $("<img>").attr({
+                                    src: "images/shop-bag.png",
+                                    alt: "bag",
+                                    id: "product-link",
+                                  })
+                                )
+                                .append($("<span>").text("sepete ekle"))
+                            )
+                          )
+                          .append(
+                            $("<li>").append(
+                              $("<a>")
+                                .addClass("btn")
+                                .attr({ href: "wishlist.html" })
+                                .append(
+                                  $("<i>")
+                                    .addClass("fa fa-heart")
+                                    .attr({ "aria-hidden": "true" })
+                                )
+                            )
+                          )
+                      )
+                  )
+              )
+          );
+        }
+      }
+
+      $(".product-info h3").remove();
+      $(".product-info ul").remove();
+      if ($(".quick-popup").length > 0) {
+        $(".quick-popup").magnificPopup({
+          type: "iframe",
+        });
+      }
+    })
+
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+};
