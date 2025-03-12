4d4dea0e456dd9bea27eca007d2b40da2c9ded38
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2025-03-12 04:59:27 +0300

    121

--- controllers/database.js
+++ controllers/database.js
@@ -3,27 +3,11 @@ const axios = require("axios");
 const asyncWrapper = require("../middleware/async");
 const { createCustomError } = require("../errors/custom-error");
 const constants = require("../constants");
+const data = require("../db/db");
 
 const getAllproducts = (req, res) => {
-  const options = {
-    method: "GET",
-    url: `https://api.shopier.com/v1/products?`,
-    headers: {
-      accept: "application/json",
-      authorization: constants.Bearer,
-    },
-    params: { limit: 50, page: 1 },
-  };
-
-  axios
-    .request(options)
-    .then((response) => {
-      const contentType = response.headers["content-type"];
-      const data = response.data;
-      console.log(data);
-      res.status(200).json({ data }); //send json object with success true and  array
-    })
-    .catch((err) => console.error(err));
+  console.log(data);
+  res.status(200).json({ data }); //send json object with success true and  array
 
   //GET
 };
