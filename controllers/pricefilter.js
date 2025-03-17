5eb71fb52f818066dafc2caa69796102609e895d
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2025-03-17 04:09:13 +0300

    12

--- controllers/pricefilter.js
+++ controllers/pricefilter.js
@@ -8,6 +8,7 @@ const womanData = require("../db/womanshop"); //woman data
 const manData = require("../db/manshop");
 const accessoriesData = require("../db/accessoriesshop");
 const bagData = require("../db/bagshop");
+const jewelryData = require("../db/jewelryshop");
 const _ = require("underscore");
 
 const getAllproducts = (req, res) => {
@@ -58,6 +59,18 @@ const getByQuery = (req, res) => {
   if (sentUrl === "bagshop") {
     // filter by price first
     const result = bagData.filter(
+      (o) => o.priceData.discountedPrice >= minPrice
+    );
+    // then sort them ascending
+    var data = _.sortBy(result, function (o) {
+      return o.priceData.discountedPrice;
+    });
+    console.log(data);
+    res.status(200).json({ data }); //send json object with success true and  array);
+  }
+  if (sentUrl === "jewelryshop") {
+    // filter by price first
+    const result = jewelryData.filter(
       (o) => o.priceData.discountedPrice >= minPrice
     );
     // then sort them ascending
